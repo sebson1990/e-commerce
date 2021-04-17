6e9398592ff4d8245a37ce07d4e2e3625decc2f3
sebson1990 <sebastian.dzieniak@mycit.ie>
2021-04-17 14:44:01 +0000

    testing the database vars

--- my_e_commerce_platfrom/settings.py
+++ my_e_commerce_platfrom/settings.py
@@ -125,19 +125,6 @@ WSGI_APPLICATION = 'my_e_commerce_platfrom.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
  
-if 'DATABASE_URL' in os.environ:
-    print("using PostgreSQL")
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    },
-else:
-    print("using SQLite3")
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
 
 
 AUTH_PASSWORD_VALIDATORS = [
